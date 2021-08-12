5bf0b9ddb852882cfbd1cb6baeaf427a84d12072
VrajPatel11 <patelvraj1132000@gmail.com>
2021-08-12 15:43:37 +0530

    Active Storage Overview

---
+++ Other Components/Active Storage Overview/discarding_files.rb
@@ -0,0 +1,38 @@
+#Discarding Files
+
+#during system tests
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def remove_uploaded_files
+    FileUtils.rm_rf("#{Rails.root}/storage_test")
+  end
+
+  def after_teardown
+    super
+    remove_uploaded_files
+  end
+end
+#to clean up attached files during system test use an after_teardown callback
+
+
+#during integration tests
+module RemoveUploadedFiles
+    def after_teardown
+      super
+      remove_uploaded_files
+    end
+  
+    private
+  
+    def remove_uploaded_files
+      FileUtils.rm_rf(Rails.root.join('tmp', 'storage'))
+    end
+  end
+  
+  module ActionDispatch
+    class IntegrationTest
+      prepend RemoveUploadedFiles
+    end
+  end
+end  
